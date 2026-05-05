6e5ea00c251858418ff554fe509ecf533c7975f8
Boros <kevstar421@gmail.com>
2026-05-05 03:19:12 -0400

    ng: add page titles

--- SalonLluvia/angular-app/src/app/app.routes.ts
+++ SalonLluvia/angular-app/src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Services } from './components/services/services';
 
+const salon: string = "- Salon Lluvia";
+
 export const routes: Routes = [
     {
         path: "",
         component: Home,
-        title: "Realza su Belleza"
+        title: `Realza su Belleza ${salon}`
     },
     {
         path: "servicio",
         component: Services,
-        title: "Servicios"
+        title: `Servicios ${salon}`
     },
 ];
