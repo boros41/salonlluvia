72cfb6dba26f63c9ea7706abe0fd9a116328f314
Boros <kevstar421@gmail.com>
2026-05-05 02:34:24 -0400

    add ng footer component

--- SalonLluvia/angular-app/src/app/components/navbar/navbar.ts
+++ SalonLluvia/angular-app/src/app/components/navbar/navbar.ts
@@ -3,8 +3,8 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'navbar',
   imports: [],
-  templateUrl: './app.navbar.html',
-  styleUrl: './app.navbar.css'
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
 })
 export class Navbar {
 
