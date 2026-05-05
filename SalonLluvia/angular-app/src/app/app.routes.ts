279c726b88a43940a9667368405ae7268536214b
Boros <kevstar421@gmail.com>
2026-05-05 03:01:26 -0400

    ng: add route to services page

--- SalonLluvia/angular-app/src/app/app.routes.ts
+++ SalonLluvia/angular-app/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './components/home/home';
+import { Services } from './components/services/services';
 
 export const routes: Routes = [
     {
         path: "",
         component: Home,
         title: "Realza su Belleza"
+    },
+    {
+        path: "servicio",
+        component: Services,
+        title: "Servicios"
     },
 ];
